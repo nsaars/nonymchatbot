f007486067ff8ea4b26ac2053c17ff7a8c7e3e4c
nsaars <mimalliktv04@gmail.com>
2022-07-25 07:36:09 +0500

    test heroku environ vars 4

--- data/config.py
+++ data/config.py
@@ -4,3 +4,4 @@ print(os.environ.items())
 print(os.environ['BOT_TOKEN'], os.environ['DATABASE_URL'])
 BOT_TOKEN = os.environ['BOT_TOKEN']
 POSTGRES_URI = os.environ['DATABASE_URL']
+ADMINS = (732928701,)
