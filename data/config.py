209b5d14c8ed285ae1227de0bed47089a5bd4e06
nsaars <mimalliktv04@gmail.com>
2022-07-25 07:50:19 +0500

    updated environ variables logic

--- data/config.py
+++ data/config.py
@@ -1,7 +1,5 @@
 import os
 
-print(os.environ.items())
-print(os.environ['BOT_TOKEN'], os.environ['DATABASE_URL'])
 BOT_TOKEN = os.environ['BOT_TOKEN']
 POSTGRES_URI = os.environ['DATABASE_URL']
 ADMINS = (732928701,)
